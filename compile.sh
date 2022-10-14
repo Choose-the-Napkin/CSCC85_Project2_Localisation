43f30b00779a9d86a7cdd325a35532f432119770
farazkaleemmalik <farazmalik17465@gmail.com>
2022-10-13 21:23:56 -0400

    Add debug.c + compile script option

--- compile.sh
+++ compile.sh
@@ -1,1 +1,6 @@
-g++ EV3_Localization.c ./EV3_RobotControl/btcomm.c -lbluetooth
+if [ "$1" = "-d" ] ; then
+    g++ debug.c ./EV3_RobotControl/btcomm.c -lbluetooth -o debug
+else
+    echo "$1"
+    g++ EV3_Localization.c ./EV3_RobotControl/btcomm.c -lbluetooth  -o localisation
+fi
---
+++ debug.c
@@ -0,0 +1,89 @@
+/* EV3 API
+ *  Copyright (C) 2018-2019 Francisco Estrada and Lioudmila Tishkina
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// Initial testing of a bare-bones BlueTooth communication
+// library for the EV3 - Thank you Lego for changing everything
+// from the NXT to the EV3!
+
+#include "./EV3_RobotControl/btcomm.h"
+#include <time.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+
+int main(int argc, char *argv[]) {
+  char test_msg[8] = {0x06, 0x00, 0x2A, 0x00, 0x00, 0x00, 0x00, 0x01};
+  char reply[1024];
+  int tone_data[50][3];
+  memset(&reply[0], 0, 1024);
+
+// just uncomment your bot's hex key to compile for your bot, and comment the
+// other ones out.
+#ifndef HEXKEY
+#define HEXKEY "00:16:53:56:56:03"  // <--- SET UP YOUR EV3's HEX ID here
+#endif
+
+  BT_open(HEXKEY);
+  printf("Connected!\n");
+  fflush(stdout);
+
+  // name must not contain spaces or special characters
+  // max name length is 12 characters
+  BT_setEV3name("R2D2");
+  BT_all_stop(0);
+  
+  int RGB[3], gyro, extended_psh, retracted_psh;
+
+  //pid_t pid = fork();
+  int pid = 0;
+  if (pid == 0) { // Child
+    int direction = 1;
+    int e, r;
+    //BT_motor_port_start(MOTOR_B, direction * 30);
+    while (1) {
+      e = BT_read_touch_sensor(PORT_4);
+      r = BT_read_touch_sensor(PORT_3);
+      if (e == 1 || r == 1) {
+        direction = e * 2 - 1;
+      }
+      
+      BT_motor_port_start(MOTOR_B, direction * 50);
+      //printf("2");
+      //fflush(stdout);
+      usleep(100*1000);
+    }
+  } else {
+    for (;;) {
+      // Print all sensor data
+      BT_read_colour_sensor_RGB(PORT_1, RGB);
+      gyro = BT_read_gyro_sensor(PORT_3);
+      extended_psh = BT_read_touch_sensor(PORT_4);
+      retracted_psh = BT_read_touch_sensor(PORT_3);
+
+      printf("Gyro: %d, Extended: %d, Retracted: %d, Color Hexcode: %d %d %d\n", gyro, extended_psh, retracted_psh, (int) ((double)RGB[0]/(305.0/256)), (int) ((double)RGB[1]/(305.0/256)), (int) ((double)RGB[2]/(305.0/256)));
+      fflush(stdout);
+      sleep(1);
+    }
+  }
+
+
+  BT_all_stop(0); // Allow for free rotation of the motors
+  BT_close();
+  fprintf(stderr, "Done!\n");
+}
