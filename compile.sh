abe59e5545672bad12c7ca68460085f21a99806b
farazkaleemmalik <farazmalik17465@gmail.com>
2022-10-27 18:02:20 -0400

    Debug flags

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 if [ "$1" = "-d" ] ; then
     g++ debug.c ./EV3_RobotControl/btcomm.c -lbluetooth -o debug
 elif [ "$1" = "" ] ; then
-    g++ EV3_Localization.c ./EV3_RobotControl/btcomm.c -lbluetooth  -o localisation
+    g++ EV3_Localization.c -g ./EV3_RobotControl/btcomm.c -lbluetooth  -o localisation
 else
     g++ $1.c ./EV3_RobotControl/btcomm.c -lbluetooth  -o $1
 fi
